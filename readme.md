a59095e6c460930a8dbca360880ec2703fda214b
Andrew Noonan <andrewnoonan87@proton.me>
2026-02-11 22:58:37 -0500

    should finally be complete

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
+# Website Rational
+I made a website about linux, because it is a topic I enjoy, and gave me an opportunity to write about it.
+## Page description
+The *about page* was just a short sentence about me, and an example to use javascript to dynamically greet based on the time.
+The *debian page* was used for giving a short overview about debian, same with the other distro pages. 
+The *index* page was used for providing an overview of linux, and a simplified table to compare the distros.
 ## Sources used in the creation of this website
 [Freecodecamp html starter](https://www.freecodecamp.org/news/html-starter-template-a-basic-html5-boilerplate-for-index-html/)
 referenced for making the basic boilerplate of the website
@@ -9,8 +15,26 @@ Tips for styling navbar
 basic little hover effect 
 [Gruvbox Color Scheme](https://github.com/morhetz/gruvbox?tab=readme-ov-file)
 Color Scheme used for this website
-[Create a Dark Mode Switch]{https://www.youtube.com/watch?v=_gKEUYarehE)
+[Create a Dark Mode Switch](https://www.youtube.com/watch?v=_gKEUYarehE)
 very useful video for creating an aesthetic dark-mode switch
 
 [MDN tables](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/table) 
 boilerplate table stuff
+[Event listener stuff](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+)
+
+## HTML Validation
+No errors or warnings!
+https://validator.w3.org/nu/#file
+## CSS Validation
+No errors were reported!
+https://jigsaw.w3.org/css-validator/validator$link
+
+## HTML Used
+For the HTML, as instructed, I tried to use the various semantic elements and used divs for unique styling. I used <header> to make the navigation bar with a nested <nav> class, where I link to all of the pages that I made.
+<buttons> were used for the various buttons I used, and <ul> was used for unordered lists. <table> was used for making a table
+
+## CSS Used
+The CSS that I used was a bit more interested. I wanted to make my website conform to my favorite colorscheme, Gruvbox. As such, I styled a bunch of the elements to abide by that colorscheme, and also created dark-mode versions that would be triggered by the button I created with in the HTML.
+## JavaScript used
+I had 6 javascript functions, with two of those being attached to event listeners to do specific things. My first two are to modify the localstorage and modify the html class names, so as to revert or reinstate the user preference from the last time the button was clicked. My themeSwitch function is essentially just calling these two functions, adding the event listener to the button. My next function is to set the dynamic greeting for the *About Me* page. My last one, which was also my last one with an eventlistener, is to cross out items in a list after they are clicked
--- script.js
+++ script.js
@@ -1,46 +1,53 @@
-const themeSwitch = document.getElementById('theme-switch');
-
 const enableDarkMode = () => {
-  document.body.classList.add('dark-mode');
-  localStorage.setItem('darkmode', 'active'); 
+  document.body.classList.add('dark-mode'); // adding dark-mode to the class names so that their style will be changed by CSS
+  localStorage.setItem('darkmode', 'active'); // setting the value in localstorage after the change occurred so that we'll be able to check for it in our later function
 };
 
 const disableDarkmode = () => {
-  document.body.classList.remove('dark-mode');
-  localStorage.setItem('darkmode', 'inactive'); 
+  document.body.classList.remove('dark-mode'); // remove dark-mode from the class names to revert back to their default 'light' style
+  localStorage.setItem('darkmode', 'inactive'); //setting the value in localstorage to check later
 };
 
+document.addEventListener('DOMContentLoaded', () => { // making the function run after all of the DOM has been loaded. Should be unneccessary, but the toggle was failing without.
+  const themeSwitch = document.getElementById('theme-switch'); // creating a variable for the id of our button 
 
-let darkmode = localStorage.getItem('darkmode');
-if (darkmode === "active") enableDarkMode();
+  let darkmode = localStorage.getItem('darkmode'); // setting darkmode = the value in local storage
+  if (darkmode === 'active') enableDarkMode(); // checking if darkmode is set to active in local storage, if so, enable it.
 
-
-themeSwitch.addEventListener("click", () => {
-  
-  darkmode = localStorage.getItem('darkmode'); 
-  
-  if (darkmode !== "active") {
-    enableDarkMode();
-  } else {
-    disableDarkmode();
+  if (themeSwitch) { // if our button exists on the page
+    themeSwitch.addEventListener('click', () => { //attaching a click listener to the button we mapped as a variable earlier
+      darkmode = localStorage.getItem('darkmode'); // checking for dark mode again
+      if (darkmode !== 'active') {
+        enableDarkMode();
+      } else {
+        disableDarkmode();
+      }
+    });
   }
-});
 
-function setDynamicGreeting() {
-  const greetingElement = document.getElementById('greeting');
-  const hour = new Date().getHours();
-  let message = "";
-  
-  if (hour < 12) {
-    message = "Good Morning! I hope you slept well";
-
-  } else if (hour < 18) {
-    message = "Good Afternoon! I hope your day has been going well.";
-
-  } else {
-    message = "Good Evening! Time to wind down.";
-
+  function setDynamicGreeting() {
+    const greetingElement = document.getElementById('greeting'); // setting greetingElement as a variable equal to the location within the DOM
+    if (!greetingElement) return; // if we can't find an element by the Id 'greeting' on the DOM for that page, exit the function(avoids error in console)
+    const hour = new Date().getHours(); // getting the current hour of the day
+    let message = '';
+    if (hour < 12) {
+      message = 'Good Morning! I hope you slept well';
+    } else if (hour < 18) {
+      message = 'Good Afternoon! I hope your day has been going well.';
+    } else {
+      message = 'Good Evening! Time to wind down.';
+    }
+    greetingElement.innerText = message; //modifying the text to our message
   }
-  greetingElement.innerText = message;
+  setDynamicGreeting(); // run the function 
+
+const listItems = document.querySelectorAll("li"); //get a list of all <li>
+
+function toggleDone(e) {
+ e.target.classList.toggle("done") //assign the class done to the object if it wasn't already marked as such, and remove it if it was
 }
-setDynamicGreeting();
+
+  listItems.forEach((item) => {
+    item.addEventListener('click', toggleDone); 
+  });
+});
